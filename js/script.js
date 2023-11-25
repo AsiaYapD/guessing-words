b3cbb6f667c47eadfd3952103292bdedca55a872
Asia Yap-Dubois <asiayapd@gmail.com>
2023-11-25 09:26:18 -0500

    Added global variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Global variables //
+
+const guessedLetters = document.querySelector(".guessed-letters");
+const button = document.querySelector(".guess");
+const typeLetter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector("remaining");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector("play-again hide");
+
+const word = "magnolia";
+
+
