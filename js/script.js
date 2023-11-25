fc5ab8f94c24a9191aa67037ad544734a434fe30
Asia Yap-Dubois <asiayapd@gmail.com>
2023-11-25 09:52:17 -0500

    Added event listener for the button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 // Global variables //
 
-const guessedLetters = document.querySelector(".guessed-letters");
-const button = document.querySelector(".guess");
-const typeLetter = document.querySelector(".letter");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector("remaining");
+const remainingGuessesElement = document.querySelector("remaining");
 const message = document.querySelector(".message");
-const playAgainButton = document.querySelector("play-again hide");
+const playAgainButton = document.querySelector("play-again");
 
 const word = "magnolia";
 
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
 
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
