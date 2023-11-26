dbc6329a093ed346178affa021db7baef5460d23
Asia Yap-Dubois <asiayapd@gmail.com>
2023-11-26 14:16:43 -0500

    Created funciotn to check and validate player's input

--- js/script.js
+++ js/script.js
@@ -9,21 +9,53 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector("play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
     const placeholderLetters = [];
     for (const letter of word) {
         console.log(letter);
         placeholderLetters.push("●");
-    }
+    };
     wordInProgress.innerText = placeholderLetters.join("");
-};
+}
 
 placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
+    //console.log(guess);
+
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    };
     letterInput.value = "";
+
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please write a letter!";
+    } else if (input.length > 1) {
+        message.innerText = "Only write one letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please write letters only!";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed this one!";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    };
+};
