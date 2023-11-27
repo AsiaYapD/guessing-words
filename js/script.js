1295f5428f8f04e864dfb32ccbed1d6af3428b23
Asia Yap-Dubois <asiayapd@gmail.com>
2023-11-27 10:28:14 -0500

    De-bugged async function

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector("play-again");
 let word = "magnolia";
 const guessedLetters = [];
-let remainingGuesses = 8; /* subject to change */
+let remainingGuesses = 8; 
 
 const getWord = async function () {
     const response = await fetch(
         "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt"
         );
-    const words = await words.text();
+    const words = await response.text();
     const wordArray = words.split("\n");
-    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
     word = wordArray[randomIndex].trim();
 
     placeholder(word);
