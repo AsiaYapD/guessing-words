d220140497d863462f0a905972f903e29b004b1f
Asia Yap-Dubois <asiayapd@gmail.com>
2023-11-28 07:15:33 -0500

    Add start over button

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const playAgainButton = document.querySelector("play-again");
+const playAgainButton = document.querySelector(".play-again");
 let word = "magnolia";
 const guessedLetters = [];
-let remainingGuesses = 8; 
+let remainingGuesses = 8;
 
 const getWord = async function () {
     const response = await fetch(
         "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt"
-        );
+    );
     const words = await response.text();
     const wordArray = words.split("\n");
     const randomIndex = Math.floor(Math.random() * wordArray.length);
@@ -114,6 +114,7 @@ const updateGuessesRemaining = function (guess) {
 
     if (remainingGuesses === 0) {
         message.innerHTML = `Game over! The word was <span class="highlight">${word}</span>.`;
+        startOver();
     } else if (remainingGuesses === 1) {
         remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
     } else {
@@ -121,13 +122,18 @@ const updateGuessesRemaining = function (guess) {
     };
 };
 
-
-
 const playerWon = function () {
     if (word.toUpperCase() === wordInProgress.innerText) {
         message.classList.add("win");
         message.innerHTML =
             `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+        startOver();
     };
 };
 
+const startOver = function() {
+    guessLetterButton.classList.add("hide");
+    remainingGuessesElement.classList.add("hide");
+    guessedLettersElement.classList.add("hide");
+    playAgainButton.classList.remove("hide");
+};
